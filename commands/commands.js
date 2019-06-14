ac942b623235dcd1d9c42688ae33635afe11777a
bigwoke <djk152@gmail.com>
2019-06-14 01:45:06 -0400

    add helper commands

---
+++ commands/commands.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js')
+const tools = require('../tools.js')
+
+module.exports.run = async (mord, msg, args) => {
+  let roles = tools.findRoles(mord, msg)
+  let color = roles ? roles.last().color : 0
+
+  let cmdEmbed = new Discord.RichEmbed()
+    .setColor(color)
+    .setAuthor('List of available Mord commands', 'http://memerust.tk/files/Mord.png')
+
+  let commands = mord.commands.sort((a, b) => {
+    if (a[0] < b[0]) return -1
+    if (a[0] > b[0]) return 1
+    return 0
+  })
+
+  if (msg.channel.type === 'dm') {
+    cmdEmbed.setFooter('Commands unavailable in DM will not be listed.')
+  }
+
+  // TODO: Add insurance for >25 commands
+
+  commands.forEach(cmd => {
+    // Check permissions here
+    if (msg.channel.type === 'dm' && !cmd.info.dm) return
+    cmdEmbed.addField(cmd.info.usage, cmd.info.desc)
+  })
+
+  msg.channel.send({ embed: cmdEmbed }).then(resp => {
+    if (msg.channel.type === 'text') {
+      resp.delete(60000)
+      msg.delete(60000)
+    }
+  })
+}
+
+module.exports.info = {
+  name: 'commands',
+  usage: `${process.env.PREFIX}commands`,
+  desc: 'Lists available commands, including usage and descriptions.',
+  dm: true
+}
---
+++ commands/info.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js')
+const tools = require('../tools.js')
+
+module.exports.run = async (mord, msg, args) => {
+  let roles = tools.findRoles(mord, msg)
+  let color = roles ? roles.last().color : 0
+
+  let info = new Discord.RichEmbed()
+    .setColor(color)
+    .setAuthor('Mord Information Panel', 'http://memerust.tk/files/Mord.png')
+    .setTitle('Mord is a multipurpose, semi-modular Discord chat bot.')
+    .setDescription('Written from scratch using Node.JS and MongoDB, Mord was ' +
+      'created as a quote-keeping bot, with other useful abilities and features.')
+    .addField('Quotekeeping', 'Quotes can be kept from multiple different Discords.')
+    .addField('Self-Clearing', 'Simple one-off responses are auto-removed to prevent spam.')
+    .addField('Private Messages', 'Commands are accepted via DM where possible, with ' +
+      'the exception of functionality that is tied to a specific server (e.g. quotes).')
+    .addField('Creator', 'DJ#8074 on Discord', true)
+    .addField('Source', '[bigwoke](https://github.com/bigwoke/) on GitHub', true)
+
+  msg.channel.send({ embed: info }).then(resp => {
+    if (msg.channel.type === 'text') {
+      resp.delete(40000)
+      msg.delete(40000)
+    }
+  })
+}
+
+module.exports.info = {
+  name: 'info',
+  usage: `${process.env.PREFIX}info`,
+  desc: 'Provides basic bot information.',
+  dm: true
+}
