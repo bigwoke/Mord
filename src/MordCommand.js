0b05d555d91b67f69b6e5d3420061e3c482fed0f
bigwoke <djk152@gmail.com>
2020-03-27 04:17:29 -0400

    add support for disabling commands

--- src/MordCommand.js
+++ src/MordCommand.js
@@ -1,8 +1,8 @@
 const { Command } = require('discord-akairo')
 
 /**
- * Extension of Command class made to apply new properties
- * that allow enabling and disabling of commands in guilds.
+ * Extension of Command class made to apply new properties/methods
+ * that allow easier messaging and enabling/disabling commands.
  * @param {string} id - Command identifier.
  * @param {CommandOptions} opts - Command initialization options.
  * @extends {Command}
@@ -13,11 +13,24 @@ class MordCommand extends Command {
 
     /**
      * Whether the command message should self-destruct.
-     * `0` for never, otherwise time in ms.
+     * Falsey for never, otherwise time in ms.
      * @type {number}
      * @default 0
      */
     this.destruct = opts.destruct || null
+
+    /**
+     * Guilds in which this command is disabled.
+     * @type {Set}
+     */
+    this.disabledIn = new Set()
+
+    /**
+     * Whether this command should be protected from disabling.
+     * @type {boolean}
+     * @default false
+     */
+    this.protected = opts.protected || false
 
     /**
      * Whether the command is enabled globally.
@@ -31,18 +44,29 @@ class MordCommand extends Command {
    * Sends a message using Akairo's `CommandUtil` send method.
    * @param {Message} message - Message object to provide context.
    * @param {StringResolvable|Object} content - Content to send as a message.
-   * @param {MessageOptions|MessageAdditions} options - Options to apply to content.
-   * @param {boolean} isReply - Whether this message is a reply to a user.
-   * @returns {Promise<(Message|Array<Message>)>} Sent message(s).
+   * @param {MessageOptions|MessageAdditions} [options={}] - Options to apply to content.
+   * @param {boolean} [isReply=false]  - Whether this message is a reply to a user.
+   * @returns {Promise<Message>} Sent message(s).
    */
-  async send (message, content, options = {}, isReply = false) {
+  send (message, content, options = {}, isReply = false) {
     if (!this.handler.commandUtil) throw new Error('CommandUtil is disabled.')
     const resp = isReply
       ? message.util.reply(content, options)
       : message.util.send(content, options)
 
-    message.util.addMessage(await resp)
+    message.util.addMessage(resp)
     return resp
+  }
+
+  get globalEnabled () {
+    return this._globalEnabled
+  }
+
+  set globalEnabled (enabled) {
+    if (typeof enabled !== 'boolean') {
+      throw new TypeError('globalEnabled must be a boolean.')
+    }
+    this._globalEnabled = enabled
   }
 }
 
---
+++ src/listeners/provider/CommandRegister.js
@@ -0,0 +1,30 @@
+const { Listener } = require('discord-akairo')
+const log = require('../../../log.js')
+
+class CommandRegister extends Listener {
+  constructor () {
+    super('commandRegister', {
+      emitter: 'commandHandler',
+      event: 'load'
+    })
+  }
+
+  exec (command) {
+    const { settings } = command.client
+    settings.items.forEach(item => {
+      const isDisabled = item.disabled[command.id]
+      if (isDisabled) command.disabledIn.add(command.id)
+    })
+
+    command.client.on('ready', () => {
+      const disabledCommands = settings.get('global', 'disabled')
+      if (disabledCommands && disabledCommands[command.id]) {
+        command.globalEnabled = false
+      }
+    })
+
+    log.verbose(`Command ${command.id} loaded.`)
+  }
+}
+
+module.exports = CommandRegister
