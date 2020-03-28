c4353070be68f8110093a8c9faa967350b0c8f43
bigwoke <djk152@gmail.com>
2020-03-27 22:28:04 -0400

    fix commandRegister and verify uniqueness of mods

--- src/listeners/provider/CommandRegister.js
+++ src/listeners/provider/CommandRegister.js
@@ -10,20 +10,28 @@ class CommandRegister extends Listener {
   }
 
   exec (command) {
-    const { settings } = command.client
-    settings.items.forEach(item => {
-      const isDisabled = item.disabled_cmd[command.id]
-      if (isDisabled) command.disabledIn.add(command.id)
-    })
-
     command.client.on('ready', () => {
+      const { settings } = command.client
+
+      settings.items.forEach(item => {
+        const isDisabled = item.disabled_cmd[command.id]
+        if (isDisabled) command.disabledIn.add(item._id)
+      })
+
       const disabledCommands = settings.get('global', 'disabled_cmd')
       if (disabledCommands && disabledCommands[command.id]) {
         command.globalEnabled = false
       }
     })
 
+    this.verifyID(command)
     log.verbose(`Command ${command.id} loaded.`)
+  }
+
+  verifyID (command) {
+    const { categories } = command.handler
+    const duplicateID = categories.some(cat => cat.id === command.id)
+    if (duplicateID) throw new Error('Category and command IDs must be unique.')
   }
 }
 
