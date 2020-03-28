e8f78a32c6fe20ab2e880f59ce308047b7632335
bigwoke <djk152@gmail.com>
2020-03-28 04:16:38 -0400

    refine category disabling/enabling, add * wildcard

--- src/commands/util/disable.js
+++ src/commands/util/disable.js
@@ -7,8 +7,9 @@ class DisableCommand extends Command {
     super('disable', {
       aliases: ['disable'],
       category: 'util',
-      description: 'Disables the specified command in the current guild,' +
-        ' or globally with the --global flag.',
+      description: 'Disables the specified command or category in the current ' +
+        'guild, or globally with the --global flag. Categories must be prefixed ' +
+        'with `category:` to distinguish them from commands.',
       destruct: 10000,
       protected: true,
       cooldown: 5000,
@@ -17,9 +18,10 @@ class DisableCommand extends Command {
       args: [
         {
           id: 'mod',
-          type: Argument.union('category', 'command', 'commandAlias'),
+          type: DisableCommand.determineType,
           prompt: {
-            start: 'Which command or category do you want to disable?',
+            start: 'Which command or category do you want to disable? ' +
+              'Categories must use `category:` flag, i.e. `category:name`.',
             retry: 'That command or category does not exist, try again.'
           }
         },
@@ -33,30 +35,22 @@ class DisableCommand extends Command {
   }
 
   exec (message, args) {
+    // Set type of module to disable and get scope of command.
     const type = args.mod instanceof Command ? 'command' : 'category'
     const scope = this.getScope(message, args)
     if (!scope) return
 
-    const responses = {
-      prot: `Sorry, the \`${args.mod.id}\` ${type} is protected and can't be disabled.`,
-      disabledGlobally: `The \`${args.mod.id}\` ${type} is already disabled globally.`,
-      warning: `\nThis ${type} is disabled globally, so in the instance that it is ` +
-        'enabled in a global scope, it will remain disabled here.',
-      success: `The \`${args.mod.id}\` ${type} is now disabled ` +
-        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`,
-      failure: `The \`${args.mod.id}\` ${type} is already disabled ` +
-        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`
-    }
-
+    const responses = DisableCommand.getResponses(message, args, type, scope)
     if (this.modIsProtected(args)) return this.send(message, responses.prot)
 
+    // Determine whether the command should be disabled, respond accordingly.
     const result = this.runLogic(message, args, scope)
     const resp = scope !== 'global' && !args.mod.globalEnabled
       ? responses[result] + responses.warning
       : responses[result]
-
     this.send(message, resp)
 
+    // If the command needs to be disabled, do it. Return otherwise.
     if (result === 'failure') return
     return this.client.settings.set(
       scope,
@@ -65,11 +59,38 @@ class DisableCommand extends Command {
     )
   }
 
+  /**
+   * Get the scope of the disable action, either global or guild.
+   * @param {Message} message - Prompting message used for context.
+   * @param {Object} args - Parsed arguments from the command.
+   * @returns {string|null} 'Global' or specific guild ID, otherwise null.
+   */
+  getScope (message, args) {
+    if (this.client.isOwner(message.author)) {
+      if (args.global) return 'global'
+      if (isDM(message)) return 'global'
+    }
+    if (message.channel.type === 'text') return message.guild.id
+    return null
+  }
+
+  /**
+   * Determine whether a command is protected or a category contains one that is.
+   * @param {Object} args - Parsed arguments from the command.
+   * @returns {boolean}
+   */
   modIsProtected (args) {
     const protectedInCategory = args.mod.some && args.mod.some(cmd => cmd.protected)
     return args.mod.protected || protectedInCategory
   }
 
+  /**
+   * Determine whether a command should be disabled, if so then set status.
+   * @param {Message} message - Prompting message used for context.
+   * @param {Object} args - Parsed arguments from the command.
+   * @param {string} scope - Where the command impacts, global or guild ID.
+   * @returns {string} 'success' or 'failure' result from disabling.
+   */
   runLogic (message, args, scope) {
     if (scope === 'global' && args.mod.globalEnabled) {
       args.mod.globalEnabled = false
@@ -82,13 +103,39 @@ class DisableCommand extends Command {
     return 'failure'
   }
 
-  getScope (message, args) {
-    if (this.client.isOwner(message.author)) {
-      if (args.global) return 'global'
-      if (isDM(message)) return 'global'
+  /**
+   * Determine whether the given argument is a command or category.
+   * @param {Message} message - Prompting message used for context.
+   * @param {string} phrase - Argument text content.
+   * @returns {Command|Category|null}
+   */
+  static async determineType (message, phrase) {
+    const { resolver } = this.handler
+    const catFlag = 'category:'
+    const cmd = await Argument.cast('commandAlias', resolver, message, phrase)
+    const cat = await Argument.cast('category', resolver, message, phrase.substr(catFlag.length))
+    return cmd || cat || null
+  }
+
+  /**
+   * Get object of responses for disabling options.
+   * @param {Message} message - Prompting message used for context.
+   * @param {Object} args - Parsed arguments from the command.
+   * @param {string} type - Argument type, either 'command' or 'category'.
+   * @param {string} scope - Where the command impacts, global or guild ID.
+   * @returns {Object}
+   */
+  static getResponses (message, args, type, scope) {
+    return {
+      prot: `Sorry, the \`${args.mod.id}\` ${type} is protected and can't be disabled.`,
+      disabledGlobally: `The \`${args.mod.id}\` ${type} is already disabled globally.`,
+      warning: `\nThis ${type} is disabled globally, so in the instance that it is ` +
+        'enabled in a global scope, it will remain disabled here.',
+      success: `The \`${args.mod.id}\` ${type} is now disabled ` +
+        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`,
+      failure: `The \`${args.mod.id}\` ${type} is already disabled ` +
+        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`
     }
-    if (message.channel.type === 'text') return message.guild.id
-    return null
   }
 }
 
--- src/commands/util/enable.js
+++ src/commands/util/enable.js
@@ -8,7 +8,9 @@ class EnableCommand extends Command {
       aliases: ['enable'],
       category: 'util',
       description: 'Enables the specified command in the current guild if ' +
-        'disabled, or globally (not in specific guilds) with the --global flag.',
+        'disabled, or globally (not in each guild) with the --global flag. ' +
+        'Categories must be prefixed with `category:` to distinguish them ' +
+        'from commands.',
       destruct: 10000,
       protected: true,
       cooldown: 5000,
@@ -17,9 +19,10 @@ class EnableCommand extends Command {
       args: [
         {
           id: 'mod',
-          type: Argument.union('category', 'command', 'commandAlias'),
+          type: EnableCommand.determineType,
           prompt: {
-            start: 'Which command or category do you want to enable?',
+            start: 'Which command or category do you want to enable? ' +
+            'Categories must use `category:` flag, i.e. `category:name`.',
             retry: 'That command or category does not exist, try again.'
           }
         },
@@ -33,34 +36,81 @@ class EnableCommand extends Command {
   }
 
   exec (message, args) {
+    // Set type of module to enable and get scope of this command.
     const type = args.mod instanceof Command ? 'command' : 'category'
     const scope = this.getScope(message, args)
     if (!scope) return
 
-    const responses = {
-      warning: `\nThis ${type} is disabled globally, so it will remain ` +
-        'unusable until it is enabled in a global scope.',
-      success: `The \`${args.mod.id}\` ${type} is now enabled for use ` +
-        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`,
-      failure: `The \`${args.mod.id}\` ${type} is already enabled for use ` +
-        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`
-    }
+    // If the user gave an 'all' wildcard (command/category), just enable all.
+    if (args.mod.includes && args.mod.includes('*')) return this.enableAll(message, args, scope)
 
+    // Get responses and put together + send the appropriate end response.
+    const responses = EnableCommand.getResponses(message, args, type, scope)
     const result = this.runLogic(message, args, scope)
     const resp = scope !== 'global' && !args.mod.globalEnabled
       ? responses[result] + responses.warning
       : responses[result]
-
     this.send(message, resp)
-    if (result === 'failure') return
 
+    // Enable the command if it should be, otherwise return.
+    if (result === 'failure') return
     return this.client.settings.set(
       scope,
       type === 'command' ? 'disabled_cmd' : 'disabled_cat',
-      { [args.mod.id]: true }
+      { [args.mod.id]: false }
     )
   }
 
+  /**
+   * Enables all of given type (cmd/cat) available to the client in the given scope.
+   * @param {Message} message - Prompting message used for context.
+   * @param {Object} args - Parsed arguments from the command.
+   * @param {string} scope - Where the command impacts, global or guild ID.
+   * @returns {Promise<Message>}
+   */
+  enableAll (message, args, scope) {
+    const type = args.mod === '*' ? 'command' : 'category'
+    const { settings } = this.client
+    const { modules, categories } = this.handler
+
+    if (type === 'command') {
+      modules.forEach(cmd => {
+        if (cmd.disabledIn.has(scope)) cmd.disabledIn.delete(message.guild.id)
+        settings.set(scope, 'disabled_cmd', { [cmd]: false })
+      })
+    }
+    if (type === 'category') {
+      categories.forEach(cat => {
+        if (cat.disabledIn.has(scope)) cat.disabledIn.delete(message.guild.id)
+        settings.set(scope, 'disabled_cat', { [cat]: false })
+      })
+    }
+
+    return this.send(message, EnableCommand.getResponses(message, args, type, scope).all)
+  }
+
+  /**
+   * Get the scope of the enabling action, either global or guild.
+   * @param {Message} message - Prompting message used for context.
+   * @param {Object} args - Parsed arguments from the command.
+   * @returns {string|null} 'Global' or specific guild ID, otherwise null.
+   */
+  getScope (message, args) {
+    if (this.client.isOwner(message.author)) {
+      if (args.global) return 'global'
+      if (isDM(message)) return 'global'
+    }
+    if (message.channel.type === 'text') return message.guild.id
+    return null
+  }
+
+  /**
+   * Determine whether a command should be enabled, if so then set status.
+   * @param {Message} message - Prompting message used for context.
+   * @param {Object} args - Parsed arguments from the command.
+   * @param {string} scope - Where the command impacts, global or guild ID.
+   * @returns {string} 'success' or 'failure' result from enabling.
+   */
   runLogic (message, args, scope) {
     if (scope === 'global' && !args.mod.globalEnabled) {
       args.mod.globalEnabled = true
@@ -73,13 +123,40 @@ class EnableCommand extends Command {
     return 'failure'
   }
 
-  getScope (message, args) {
-    if (this.client.isOwner(message.author)) {
-      if (args.global) return 'global'
-      if (isDM(message)) return 'global'
+  /**
+   * Determine whether the given argument is a command, category, or all wildcard.
+   * @param {Message} message - Prompting message used for context.
+   * @param {string} phrase - Argument text content.
+   * @returns {Command|Category|string|null}
+   */
+  static async determineType (message, phrase) {
+    const { resolver } = this.handler
+    const catFlag = 'category:'
+    const cmd = await Argument.cast('commandAlias', resolver, message, phrase)
+    const cat = await Argument.cast('category', resolver, message, phrase.substr(catFlag.length))
+    const all = await Argument.cast(['*', 'category:*'], resolver, message, phrase)
+    return cmd || cat || all || null
+  }
+
+  /**
+   * Get object of responses for enabling options.
+   * @param {Message} message - Prompting message used for context.
+   * @param {Object} args - Parsed arguments from the command.
+   * @param {string} type - Argument type, either 'command' or 'category'.
+   * @param {string} scope - Where the command impacts, global or guild ID.
+   * @returns {Object}
+   */
+  static getResponses (message, args, type, scope) {
+    return {
+      warning: `\nThis ${type} is disabled globally, so it will remain ` +
+      'unusable until it is enabled in a global scope.',
+      success: `The \`${args.mod.id}\` ${type} is now enabled for use ` +
+        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`,
+      failure: `The \`${args.mod.id}\` ${type} is already enabled for use ` +
+        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`,
+      all: `Any disabled ${type} modules have now been enabled ` +
+        `${scope === 'global' ? 'globally' : `in ${message.guild.name}`}.`
     }
-    if (message.channel.type === 'text') return message.guild.id
-    return null
   }
 }
 
