6ed17538473a79029070a32ebab0338a2f86be38
bigwoke <djk152@gmail.com>
2020-05-05 21:25:30 -0400

    Rework prefix command for more usability

--- src/commands/util/prefix.js
+++ src/commands/util/prefix.js
@@ -1,5 +1,6 @@
 const Command = require('../../types/MordCommand.js')
 const cfg = require('../../../config.js')
+const { isDM } = require('../../helpers/Tools.js')
 
 class PrefixCommand extends Command {
   constructor () {
@@ -7,10 +8,9 @@ class PrefixCommand extends Command {
       aliases: ['prefix'],
       category: 'util',
       description: 'Displays or changes the current prefix.',
-      destruct: 5000,
+      destruct: 10000,
       cooldown: 5000,
       ratelimit: 2,
-      userPermissions: 'ADMINISTRATOR',
       args: [
         {
           id: 'prefix'
@@ -25,35 +25,92 @@ class PrefixCommand extends Command {
   }
 
   exec (message, args) {
+    const action = this.getAction(message, args)
+
+    // If a prefix needs to be set or changed, call setPrefixes
+    if (action.includes('set')) this.setPrefixes(message, args, action)
+
+    this.sendMessage(message, args, action, args.prefix === 'default')
+  }
+
+  getAction (message, args) {
+    const isOwner = this.client.isOwner(message.author)
+
+    // If no new prefix is given, return current prefix in some scope.
+    if (args.prefix === null) {
+      return args.global || isDM(message) ? 'returnGlobal' : 'returnGuild'
+    }
+
+    // If global scope, set global prefix if owner or return it if not.
+    if (args.global || isDM(message)) {
+      return isOwner ? 'setGlobal' : 'returnGlobal'
+    }
+
+    // In guild scope, if member has permission, set prefix.
+    if (isOwner || message.member.hasPermission('MANAGE_MESSAGES')) {
+      return 'setGuild'
+    }
+
+    // Otherwise, just return the guild prefix.
+    return 'returnGuild'
+  }
+
+  sendMessage (message, args, action, isReset) {
     const { settings } = this.client
-    const { guild } = message
-    const current = settings.get(args.global ? 'global' : guild.id, 'prefix', null)
-    if (args.global) {
-      switch (args.prefix) {
-        case null:
-          this.send(message, `Global prefix is currently: \`${current}\``)
-          break
-        case 'default':
-          settings.set('global', 'prefix', cfg.client.prefix)
-          this.send(message, `Global prefix reset to default: \`${cfg.client.prefix}\``)
-          break
-        default:
-          settings.set('global', 'prefix', args.prefix)
-          this.send(message, `Global prefix has been set to: \`${args.prefix}\``)
+    const currentGlobal = this.client.settings.get('global', 'prefix')
+
+    // If the message has a guild, set currentPrefix and name to proper values
+    const [currentPrefix, name] = message.guild
+      ? [
+        settings.get(message.guild.id, 'prefix') || currentGlobal,
+        message.guild.name
+      ]
+      : [null, null]
+
+    const responses = {
+      returnGlobal: `Global prefix is currently: \`${currentGlobal}\``,
+      returnGuild: `Prefix in \`${name}\` is currently: \`${currentPrefix}\``,
+      setGlobal: `Global prefix has been set to: \`${args.prefix}\``,
+      setGuild: `Prefix in \`${name}\` is now: \`${args.prefix}\``,
+      resetGlobal: `Global prefix reset to default: \`${cfg.client.prefix}\``,
+      resetGuild: `Prefix in \`${name}\` reset to: \`${currentGlobal}\``,
+      info: '\nUsers with the "Manage Messages" permission can change this.'
+    }
+
+    if (['setGlobal', 'setGuild'].includes(action) && isReset) {
+      action = `re${action}`
+    }
+
+    let response = responses[action]
+
+    // If the current guild prefix is the same as global, append relevant text.
+    response += action === 'returnGuild' && currentPrefix === currentGlobal
+      ? ' (same as global)'
+      : ''
+
+    // If the user lacks permission to change prefix, append appropriate info.
+    response += action === 'returnGuild' && args.prefix
+      ? response += responses.info
+      : ''
+
+    this.send(message, response)
+  }
+
+  setPrefixes (message, args, action) {
+    const { settings } = this.client
+    const guildID = message.guild ? message.guild.id : null
+
+    if (action === 'setGlobal') {
+      if (args.prefix === 'default') {
+        settings.set('global', 'prefix', cfg.client.prefix)
+      } else {
+        settings.set('global', 'prefix', args.prefix)
       }
-    } else {
-      switch (args.prefix) {
-        case null:
-          this.send(message, `Prefix in \`${guild.name}\` is currently: \`${current}\``)
-          break
-        case 'default':
-          settings.delete(guild.id, 'prefix')
-          this.send(message, `Prefix in \`${guild.name}\` reset to: \`${args.prefix}\``)
-          break
-        default:
-          settings.set(guild.id, 'prefix', args.prefix)
-          this.send(message, `Prefix in \`${guild.name}\` is now: \`${args.prefix}\``)
-          break
+    } else if (action === 'setGuild') {
+      if (args.prefix === 'default') {
+        settings.delete(guildID, 'prefix')
+      } else {
+        settings.set(guildID, 'prefix', args.prefix)
       }
     }
   }
