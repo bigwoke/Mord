94bdd12db41ab34a318636e81a0d536be7001892
bigwoke <djk152@gmail.com>
2020-05-07 23:23:41 -0400

    Fix an inhibitor that prevented non-owners from using DM commands

--- src/inhibitors/commands/InsufficientPermissions.js
+++ src/inhibitors/commands/InsufficientPermissions.js
@@ -8,7 +8,8 @@ class InsufficientPermissionsInhibitor extends Inhibitor {
   }
 
   exec (message, command) {
-    if (!message.member && !this.client.isOwner(message.author)) return true;
+    if (!message.member && command.userPermissions &&
+      !this.client.isOwner(message.author)) return true;
     if (message.member && command.userPermissions &&
       !message.member.hasPermission(command.userPermissions)) return true;
   }
