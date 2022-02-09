a024ac0be405dd632dc0bb86926232c35060dddd
bigwoke <12602980+bigwoke@users.noreply.github.com>
2022-02-09 18:30:52 -0500

    fix crash on quotecount command if no quotes in db

--- src/commands/quotes/quotecount.js
+++ src/commands/quotes/quotecount.js
@@ -23,6 +23,12 @@ class QuoteCountCommand extends Command {
     const count = await this.client.data.getQuoteCount(message.guild);
     const latest = await this.client.data.getLatestQuote(message.guild);
     const { name } = message.guild;
+
+    if (!latest || !latest.number) {
+      this.send(message, `No quotes registered in ${name}!`);
+      return;
+    }
+
     const response =
       `Total quotes registered so far in ${name}: **${count}**` +
       `\nLatest (highest) quote number: **${latest.number}**`;
